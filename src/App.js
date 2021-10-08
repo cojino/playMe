6787a9dfd88277d5107137a74ba9802a089f3d67
cojino <aymanelgad65@gmail.com>
2021-10-08 03:16:43 +0100

    second try

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-import React, { useState } from "react";
-import Form from "./components/Form";
-import TodoList from "./components/TodoList";
-
 import "./App.css";
 
 function App() {
-  const [text, setText] = useState("");
-  const [todos, setTodos] = useState([]);
-  return (
-    <div className="App">
-      <Form todos={todos} setTodos={setTodos} text={text} setText={setText} />
-      <TodoList todos={todos} text={text} />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
